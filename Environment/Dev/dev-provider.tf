94d470bfab063bf8d5f31e7de9f4bc2838756442
vsaini1234-ops <vsaini.1234@gmail.com>
2026-09-01 23:34:59 +0530

    storage acoount added

--- Environment/Dev/dev-provider.tf
+++ Environment/Dev/dev-provider.tf
@@ -5,7 +5,17 @@ terraform {
       version = ">4.1.0"
     }
   }
+backend "azurerm" {
+  resource_group_name = "remote-state-rg"
+  storage_account_name = "statestorage08"
+  container_name = "statecontainer"
+  key = "stateremote.tfstate"
+
 }
+
+}
+
+
 
 
 provider "azurerm" {
